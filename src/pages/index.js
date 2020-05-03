ecb88d0ed412952614b193b183959e2084d859f0
mrtdmrmrt <mert.demir96@hotmail.com>
2020-05-03 18:29:13 +0300

    An sayfaya fotograflar eklendi

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,34 @@
 import React from "react"
-
-
-const IndexPage = () => (
-  <div>
+import {graphql} from "gatsby"
+import Img from "gatsby-image"
+import './index.css'
+const IndexPage = ({data}) => {
+  console.log(data)
+  return <div>
     <h1>Front End</h1>
+    <div className='photos'>
+      {data.allFile.edges.map(({node})=>{
+        return <Img key={node.id} fluid={node.childImageSharp.fluid}/>
+      })}
+    </div>
   </div>
-)
+}
 
+export const pageQuery = graphql`
+{
+  allFile(filter: {absolutePath: {regex: "//photos//"}}) {
+    edges {
+      node {
+        id
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  }
+}
+
+`
 export default IndexPage
